74acce7d2eb5531a3bae78cec78b4a259083c3c2
关于 <372301467@qq.com>
2024-02-04 18:18:34 +0800

    feat: init fetch rom

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -7,10 +7,12 @@ import { init_file_on_launch } from './ss/file'
 import { App } from './ui'
 import './test'
 
-init_file_on_launch()
+;(async function() {
+  await init_file_on_launch()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  )
+})()
--- src/app/ss/file.ts
+++ src/app/ss/file.ts
@@ -12,20 +12,44 @@ const set_file_handle = (handle: FileSystemFileHandle | null) => {
 }
 
 export
-const init_file_on_launch = () => {
+const init_file_on_launch = async () => {
   console.log('initialize file_handle on launch')
-  const lq = window.launchQueue
-  if (!lq) {
-    console.error('window.launchQueue not supported')
-    return
+  
+  /**
+   * 通过双击 rom 文件启动
+   * @returns 是否已 launched
+   */
+  function launch_by_click_file() {
+    const lq = window.launchQueue
+    if (!lq) {
+      console.error('window.launchQueue not supported')
+      return false
+    }
+
+    let launched = false
+    lq.setConsumer(params => {
+      const file = params.files[0]
+      if (file) {
+        console.log('launch by clicking file')
+        set_file_handle(file)
+        launched = true
+      } else
+        console.error('If there\'s no file, shouldn\'t this arrow function be executed')
+    })
+
+    return launched
   }
 
-  lq.setConsumer(params => {
-    const file = params.files[0]
-    if (file) {
-      console.log('launch by clicking file')
-      set_file_handle(file)
-    } else
-      console.error('If there\'s no file, shouldn\'t this arrow function be executed')
-  })
+  async function launch_by_download() {
+    const params = new URLSearchParams(window.location.search)
+    const rom = params.get('rom')
+    if (!rom) return false
+
+    const res = await fetch(rom)
+    // res.blob()
+    return false
+  }
+
+  let launched = launch_by_click_file()
+  launched = await launch_by_download()
 }
