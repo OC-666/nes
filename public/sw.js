99853319276bf8909df8bd06e457a54423d00aae
PPz <372301467@qq.com>
2024-02-08 14:18:13 +0800

    feat: retroarch files

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,6 @@
 // https://learn.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/#step-5---add-a-service-worker
 
-self.addEventListener('fetch', () => {})
+self.addEventListener('fetch', evt => {
+  console.log(evt.request.url)
+  console.log(location.href)
+})
