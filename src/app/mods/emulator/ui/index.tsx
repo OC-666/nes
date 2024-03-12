2a32276b16fdb6afbe2168612f610452e41fd5b9
PPz <372301467@qq.com>
2024-03-12 20:17:01 +0800

    fix: #1

--- src/app/mods/emulator/ui/index.tsx
+++ src/app/mods/emulator/ui/index.tsx
@@ -1,13 +1,27 @@
+import { useEffect } from 'react'
+
+import { useUnconfirmed_core, useHas_rom } from '../../../ss/rom'
+import { pages } from '../../../ss/pages'
+
 import { Game } from '../mods/game/ui'
 import { Game_menu } from '../mods/menu/ui'
-import { useUnconfirmed_core } from '../../../ss/rom'
 import { Select_core } from './select_core'
 
 export
 const Page_emulator = () => {
   console.log('rendering emulator, which shouldnt be too frequently...')
 
+  const nav_home = pages.home.useNav()
+  const has_rom = useHas_rom()
+  useEffect(() => {
+    if (!has_rom) {
+      console.error('no rom???')
+      nav_home()
+    }
+  }, [])
+
   const core_unconfirmed = useUnconfirmed_core()
+
   return core_unconfirmed
     ? <Select_core />
     : <>
